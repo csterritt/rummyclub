96771b670d0bff4c21a9f975e9c3120d7bd4ac8d
csterritt <csterritt@gmail.com>
2019-02-23 11:34:14 -0500

    Initial HandSort.

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	deck := rummyclub.MakeDeck()
 	rummyclub.Shuffle(deck)
 	hand := deck[0:14]
+	rummyclub.HandSort(hand)
 	fmt.Printf("%d cards in the deck, %d in the hand:\n", len(deck), len(hand))
 	for _, card := range hand {
 		fmt.Printf("%s   ", card)
